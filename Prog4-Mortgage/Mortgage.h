ce5a80593c0b056b6369135bfb98803e0f360e95
MadiSlocum <47612229+MadiSlocum@users.noreply.github.com>
2019-02-19 15:19:09 -0600

    Update Mortgage.h

--- Prog4-Mortgage/Mortgage.h
+++ Prog4-Mortgage/Mortgage.h
@@ -1,3 +1,34 @@
-#pragma once
-// header file for Mortgage class
+#pragma once //keep this here
+
+#include <iostream> 
+
+
+
+class Mortgage{
+  
+  private:
+  
+  double loan; //dollar amount of the loan
+  double interest; //annual interest rate 
+  int years; //years of the loan
+  
+  
+  
+  public:
+  
+ Mortgage(){  //constructor to set private data to zero
+   loan = 0;
+   interest = 0;
+   years = 0;
+   
+ }
+  
+  
+  
+  
+  
+  
+  
+  
+};
 
