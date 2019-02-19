6523280289ee4827e40516e2c8ca2e30316de9ad
MadiSlocum <47612229+MadiSlocum@users.noreply.github.com>
2019-02-19 12:44:42 -0600

    Update main.cpp

--- Prog4-Mortgage/main.cpp
+++ Prog4-Mortgage/main.cpp
@@ -1,3 +1,26 @@
+#include <iostream>
+#include <string>
+#include "Mortgage.h"
 
-// main goes here. Code that *uses* the Mortgage class
-// goes in this file
+int main()
+{
+  
+  Mortgage m; //mortgage instance
+  
+  m. //loan // dollar amount of loan used in calculation
+  m. //rate // annual interest rate used in calculation
+  m. //years // the number of years of the loan used in calculation
+  
+  
+ 
+  
+  cout << "Monthly payment amount: " << endl;
+  cout << //member func m.getmonthlypayment
+  
+    
+    
+  cout << "Total amount paid to the bank at the end of the loan period: "<< endl;
+  cout << //member func m.get
+  
+  return 0;
+}
