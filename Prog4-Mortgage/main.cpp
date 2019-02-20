86d492b0c1de0e36b32c2f400524232f8db962a6
MadiSlocum <47612229+MadiSlocum@users.noreply.github.com>
2019-02-20 12:40:36 -0600

    Update main.cpp

--- Prog4-Mortgage/main.cpp
+++ Prog4-Mortgage/main.cpp
@@ -1,32 +1,45 @@
 #include <iostream>
 #include <string>
+#include <cmath>
 #include "Mortgage.h"
 
 using namespace std;
+
 
 
 int main()
 {
   
   Mortgage m; //mortgage instance
+  double mLoan, mRate, monthPay, term, totalPay;
+  int mYears;
   
+
   cout << "Enter Loan Amount: " << endl;
-  cin >>   m. //loan // dollar amount of loan used in calculation
+  cin >> mLoan;//loan // dollar amount of loan used in calculation
+  m.setLoan(mLoan);
+
   cout << "Enter Rate Amount: " << endl;
-  cin >> m. //rate // annual interest rate used in calculation
+  cin >> mRate; //rate // annual interest rate used in calculation
+  m.setInterest(mRate);
+
   cout << "Enter Number of Years: " << endl;
-  cin >> m. //years // the number of years of the loan used in calculation
-  
-  
- 
-  
+  cin >> mYears; //years // the number of years of the loan used in calculation
+  m.setYears(mYears);
+
+  term = pow((1 + (mRate / 12)), (12 * mYears));
+  monthPay = ((mLoan * (mRate / 12) * term) / (mYears - 1));
+
   cout << "Monthly payment amount: " << endl;
-  cout << //member func m.getmonthlypayment
+  cout << monthPay;//member func m.getmonthlypayment
   
     
-    
-  cout << "Total amount paid to the bank at the end of the loan period: "<< endl;
-  cout << //member func m.get
-  
+
+  cout << "\nTotal amount paid to the bank at the end of the loan period: "<< endl;
+  totalPay = (monthPay * mLoan); 
+  cout << totalPay << endl;
+
+   
+
   return 0;
 }
