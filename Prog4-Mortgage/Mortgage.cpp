4417f4f925d0c56292115a7ff0dd5bcd2dc7a2e1
MadiSlocum <47612229+MadiSlocum@users.noreply.github.com>
2019-02-20 12:39:56 -0600

    Update Mortgage.cpp

--- Prog4-Mortgage/Mortgage.cpp
+++ Prog4-Mortgage/Mortgage.cpp
@@ -1,1 +1,54 @@
-// implementation file for Mortgage class
+#include "Mortgage.h"
+#include <iostream>
+
+Mortgage::Mortgage() { //constructor to initialize private data to 0
+
+	loan = 0.0;
+	interest = 0.0;
+	years = 0;
+
+}
+//=====================set
+void Mortgage::setLoan(double l)
+{
+	if (loan > 0)
+	{
+		loan = l;
+	}
+}
+
+void Mortgage::setInterest(double i)
+{
+	if (interest > 0) {
+		interest = i;
+	}
+
+}
+
+void Mortgage::setYears(int y) {
+
+	if (years > 0)
+	{
+		years = y;
+	}
+}
+
+//=====================getter func
+double Mortgage::getLoan() const
+{
+	return loan;
+}
+
+double Mortgage::getInterest() const
+{
+	return interest;
+
+}
+
+int Mortgage::getYears() const {
+
+	return years;
+}
+
+
+
